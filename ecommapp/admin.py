5ddc173e90d7d81128158a76f71f4e5787989010
Bryan Arias <bryan.arias@gmail.com>
2020-09-16 20:44:32 -0500

    Cupones, Estado_Pedido, Categorias

--- ecommapp/admin.py
+++ ecommapp/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from ecommapp.models import cupon
+from ecommapp.models import cupon, estado_pedido, categoria
 
 class cuponAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'descripcion', 'descuento' )
 
+class estado_pedidoAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', )
+
+class categoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+
 admin.site.register(cupon, cuponAdmin)
+admin.site.register(estado_pedido, estado_pedidoAdmin)
+admin.site.register(categoria, categoriaAdmin)
--- ecommprj/urls.py
+++ ecommprj/urls.py
@@ -28,5 +28,6 @@ router.register(r'categorias', views.CategoriaViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
 ]
