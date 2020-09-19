ae051b5cce7a02bc30c7ff462fcc9ca5d83bdb36
Bryan Arias <bryan.arias@gmail.com>
2020-09-19 09:25:30 -0500

    Swagger

--- ecommprj/urls.py
+++ ecommprj/urls.py
@@ -19,21 +19,22 @@ from django.urls import include, path
 from rest_framework import routers
 from ecommapp import views
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.conf.urls import url
-schema_view = get_schema_view(   
-    openapi.Info(      
-        title="PachaQtec Hackaton Final Grupo 2",     
-        default_version='v1',      
-        description="Descripcion de APIS",      
-        terms_of_service="https://www.google.com/policies/terms/",      
-        contact=openapi.Contact(email="contact@snippets.local"),      
-        license=openapi.License(name="BSD License"),   
-    ),   
-    public=True,   
-    permission_classes=(permissions.AllowAny,),
-    )
+
+#from drf_yasg.views import get_schema_view
+#from drf_yasg import openapi
+#from django.conf.urls import url
+#schema_view = get_schema_view(   
+#    openapi.Info(      
+#        title="PachaQtec Hackaton Final Grupo 2",     
+#        default_version='v1',      
+#        description="Descripcion de APIS",      
+#        terms_of_service="https://www.google.com/policies/terms/",      
+#        contact=openapi.Contact(email="contact@snippets.local"),      
+#        license=openapi.License(name="BSD License"),   
+#    ),   
+#    public=True,   
+#    permission_classes=(permissions.AllowAny,),
+#    )
 
 router = routers.DefaultRouter()
 router.register(r'cupones', views.CuponViewSet)
@@ -51,7 +52,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),    
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),    
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),    
+#    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),    
+#    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
